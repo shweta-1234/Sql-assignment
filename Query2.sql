c176da27bae205c450a6c996abacca71ee27a1a8
shweta-1234 <74421192+shweta-1234@users.noreply.github.com>
2022-03-30 12:09:02 +0530

    Add files via upload

---
+++ Query2.sql
@@ -0,0 +1,6 @@
+select * from student
+Insert into student(id,first_name,last_name,contact_no,email)
+values('1234','Shweta','Jaiswal','5678903452','shweta123@gmail.com'),
+('3456','Shreya','jain','8907652341','shreya378@gmail.com'),
+('5678','shivani','yadav','7890345623','shivani789@gmail.com');
+
---
+++ sq.sql
@@ -0,0 +1,7 @@
+create table student(
+id serial primary key,
+	first_name varchar(40),
+	last_name varchar(40),
+	contact_no varchar(40),
+	email varchar(50)	
+)
